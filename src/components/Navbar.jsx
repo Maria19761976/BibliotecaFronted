f31dafdae46e0e12ffc8333c9c064ea3eede335d
David-Navarro-Oliver <navarropalle@gmail.com>
2026-04-10 15:22:28 +0200

    style(frontend): connect navbar styling and polish shared cards

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
     return (
-        <nav>
-            <h2>Biblioteca</h2>
-            <ul>
+        <nav className="navbar">
+            <h2 className="navbar__brand">Biblioteca</h2>
+            <ul className="navbar__links">
                 <li><Link to="/authors">Autores</Link></li>
                 <li><Link to="/books">Libros</Link></li>
             </ul>

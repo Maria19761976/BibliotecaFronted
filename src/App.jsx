7952d952b7aca8e9b34047cc086846b923101f12
javiertunsi7 <javiertunsi7@gmail.com>
2026-04-10 10:41:15 +0200

    Imports and resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import './styles/global.css'
 import AuthorList from "./pages/authors/AuthorList";
 import AuthorForm from "./pages/authors/AuthorForm";
 import BookList from "./pages/books/BookList";
 import BookForm from "./pages/books/BookForm";
 
 function App() {
-  return (
+    return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/authors" element={<AuthorList />} />
-        <Route path="/authors/new" element={<AuthorForm />} />
-        <Route path="/authors/edit/:id" element={<AuthorForm />} />
-        <Route path="/books" element={<BookList />} />
-        <Route path="/books/new" element={<BookForm />} />
-        <Route path="/books/edit/:id" element={<BookForm />} />
-      </Routes>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/authors" element={<AuthorList />} />
+    <Route path="/authors/new" element={<AuthorForm />} />
+    <Route path="/authors/edit/:id" element={<AuthorForm />} />
+    <Route path="/books" element={<BookList />} />
+    <Route path="/books/new" element={<BookForm />} />
+    <Route path="/books/edit/:id" element={<BookForm />} />
+    </Routes>
     </BrowserRouter>
-  );
+    );
 }
 
 export default App;
-=======
-import Home from './pages/Home'
-import './styles/global.css'
-
-export default function App() {
-  return <Home />
-}
->>>>>>> feat/index
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
 
 function Navbar() {
@@ -14,20 +13,3 @@ function Navbar() {
 }
 
 export default Navbar;
-=======
-import './Navbar.css'
-
-export default function Navbar() {
-  return (
-    <header className="navbar">
-      <h1>La Biblioteca</h1>
-
-      <nav>
-        <span>Inicio</span>
-        <span>Autor</span>
-
-      </nav>
-    </header>
-  )
-}
->>>>>>> feat/index

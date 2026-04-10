c895e5f090dd5b1870dfed69ac09347d9e935a50
David-Navarro-Oliver <navarropalle@gmail.com>
2026-04-10 15:22:05 +0200

    chore(tooling): fix eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,23 @@
+import reactRefresh from 'eslint-plugin-react-refresh';
+
 export default [
   {
     ignores: ['dist', 'node_modules'],
+  },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
     rules: {
       'react-refresh/only-export-components': 'warn',
     },

f06131c9e561955a426341bcba4b180dd3eb5ff0
Suso777 <xesussuarez@gmail.com>
2026-04-11 19:13:20 +0200

    fix(api): use VITE_API_URL environment variable for backend endpoints

--- src/services/authorService.js
+++ src/services/authorService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/authors";
+const baseUrl = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const API_URL = `${baseUrl}/authors`;
 
 export const getAllAuthors = async() => {
     const response = await axios.get(API_URL);
--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/books";
+const baseUrl = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const API_URL = `${baseUrl}/books`;
 
 
 

e898704d266dfde17e335103c294e3334299a32e
Facundo Garavaglia Olivera <facundogo.garavagliaolivera@gmail.com>
2026-04-11 13:53:17 +0200

    Update bookService.js

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/books";
 
-
-
 export const getAllBooks = async() => {
     const response = await axios.get(API_URL);
     return response.data;
